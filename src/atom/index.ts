1da8a50602dff710a45c18cb899a2270e8952513
zhouyu21 <zhouyu21@58.com>
2023-04-04 23:04:41 +0800

    add

--- src/atom/index.ts
+++ src/atom/index.ts
@@ -2,4 +2,4 @@ import { atom } from "jotai";
 export const openKeysVal = atom<string[]>(["/pdf"]);
 export const selectedKeysVal = atom<string[]>(["/pdf/get-url-list"]);
 
-export const collapsedVal = atom<boolean>(false)
+export const collapsedVal = atom<boolean>(false);
